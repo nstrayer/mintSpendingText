e116060b09f47903f945484ff6d0d00d8446985b
Nick Strayer <nstrayer@uvm.edu>
2015-12-30 14:10:35 -0500

    Now filters out the transactions that are just moving money around, also the amount to average spending is dynamic with the past months behavior.

--- budgetApp.py
+++ budgetApp.py
@@ -13,7 +13,7 @@ mailUsername  = open('mailUsername.txt', 'r').readline().rstrip()
 phoneNumber   = open('phoneNumber.txt', 'r').readline().rstrip()
 
 #log into mint
-mint     = mintapi.Mint(mintEmail, mintPass)
+mint = mintapi.Mint(mintEmail, mintPass)
 
 # Grab the the users transactions
 transactions = mint.get_transactions()
@@ -23,17 +23,37 @@ from datetime import date, timedelta
 yesterday = date.today() - timedelta(days=1)
 dayBefore = yesterday - timedelta(days=1)
 
-# Find what was spent yesterday
-mask = (transactions['date'] > dayBefore) & (transactions['date'] <= yesterday)
-todaysSpending = transactions.loc[mask]
-spent = todaysSpending.amount.sum()
+# Ignore Chase Epay, transfer/credit card payment transactions, and rent payments.
+typeMask = (transactions['description'] != "Chase Epay") & (transactions['description'] != "Bell Midtown Pmts") \
+    & (transactions['category'] != "credit card payment") & (transactions['category'] != "transfer")
 
+#First get rid of the transactions we don't want
+totalSpending = transactions.loc[typeMask]
+
+# Find what was spent yesterday.
+timeMask = (totalSpending['date'] > dayBefore) & (totalSpending['date'] <= yesterday)
+
+#Then filter to just yesterday
+yesterdaysSpending = totalSpending.loc[timeMask]
+
+#grab just the total of how much was spent.
+spent = yesterdaysSpending.amount.sum()
 
 monthDays = [31,28,31,30,31,30,31,31,30,31,30,31] #days in the month
 #            j  f  m  a  m  j  j  a  s  o  n  d
 monthlySpending = 1000 #budget for non-rent costs per month.
 daysInMonth     = monthDays[yesterday.month-1] #grab how many days are in the month
-dailySpending   = monthlySpending/daysInMonth #how much can I spend per day on average?
+
+#find how much has been spent this month so far.
+beginingOfMonth = yesterday - timedelta(days= (yesterday.day - 1) )
+monthMask       = (totalSpending['date'] > beginingOfMonth) & (totalSpending['date'] <= yesterday)
+monthSpending   = totalSpending.loc[monthMask].amount.sum()
+
+#Subtract this amount from my desired spending amount
+budgetLeft = monthlySpending - monthSpending
+
+#calculate how much on average I need to spend for the remaining days.
+dailySpending = budgetLeft/(daysInMonth - yesterday.day)
 
 
 # # Stuff to send message
@@ -46,7 +66,7 @@ def sendText(amount) :
     '''
     Function takes the dollar amount spent and sends a text message to me with it.
     '''
-    msg = ('\nHey Nick,\n You spent $' + str(amount) + 'yesterday.\n To meet your savings goal you should have spent less than ' + str(dailySpending) + '.')
+    msg = ('You spent $' + str(amount) + ' yesterday.\n To meet savings goal you should spend less than $' + str(dailySpending) + ' for the rest of the month.')
 
     # The actual mail send
     server = smtplib.SMTP('smtp.gmail.com:587')
